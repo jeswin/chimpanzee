88b9028fea7efb9fd174072824ca4ebc643b3b89
Jeswin Kumar <jeswinpk@agilehead.com>
2017-03-04 19:57:26 +0530

    Add test for composite skip.

---
+++ src/test/fixtures/composite-skip/fixture.js
@@ -0,0 +1,21 @@
+import { composite, capture, literal } from "../../../chimpanzee";
+
+export const input = {
+  node: {
+    jeff: "buckley",
+    hello: "world",
+  },
+  prop: "something"
+}
+
+export const schema = composite(
+  {
+    something: "else",
+    hello: capture({ key: "first" }),
+    prop: literal("nothing", { key: "second", options: "alt" })
+  },
+  [
+    { name: "default", modifiers: { object: x => x.node } },
+    { name: "alt" },
+  ]
+)
--- src/test/test.js
+++ src/test/test.js
@@ -47,6 +47,7 @@ describe("chimpanzee", () => {
     ['capture-parent-child', 'capture-parent-child', "return"],
     ['capture-simple', 'capture-simple', "return"],
     ['composite', 'composite', "return"],
+    ['composite-skip', 'composite-skip', "skip"],
     ['deep', 'deep', "return"],
     ['empty', 'empty', "return"],
     ['empty-negative', 'empty-negative', "skip"],
