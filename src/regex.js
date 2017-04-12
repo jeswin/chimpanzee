223d89e7a1efe8abacdf25d91fe5ab0acf84e555
Jeswin Kumar <jeswinpk@agilehead.com>
2017-04-12 09:22:41 +0530

    Move regex away from waitForSchema

--- src/regex.js
+++ src/regex.js
@@ -2,36 +2,33 @@ import { captureIf } from "./capture";
 import { Match, Empty, Skip, Fault } from "./results";
 import Schema from "./schema";
 import { waitForSchema } from "./utils";
+import { runToResult } from "./utils";
 
-export function regex(regex, params) {
+export function regex(regex, params = {}) {
   const meta = { type: "regex", regex, params };
 
-  params = typeof params === "string" ? { key: params } : params;
-
-  function fn(obj, context, key, parents, parentKeys) {
-    return waitForSchema(
-      captureIf(
-        obj =>
-          typeof regex === "string"
-            ? typeof obj === "string" && new RegExp(regex).test(obj)
-            : typeof obj === "string" && regex.test(obj),
-        params
-      ),
-      obj,
-      context,
-      key,
-      parents,
-      parentKeys,
-      result =>
-        result instanceof Skip
-          ? new Skip(
-              `Did not match regex.`,
-              { obj, context, key, parents, parentKeys },
-              meta
-            )
-          : result
-    );
-  }
+  const fn = runToResult(params, params =>
+    (obj, context, key, parents, parentKeys) => ({
+      runner: result =>
+        () =>
+          result instanceof Skip
+            ? new Skip(
+                `Did not match regex.`,
+                { obj, context, key, parents, parentKeys },
+                meta
+              )
+            : result,
+      init: next =>
+        next(
+          captureIf(
+            obj =>
+              typeof regex === "string"
+                ? typeof obj === "string" && new RegExp(regex).test(obj)
+                : typeof obj === "string" && regex.test(obj),
+            params
+          )
+        )
+    }));
 
   return new Schema(fn, params);
 }
