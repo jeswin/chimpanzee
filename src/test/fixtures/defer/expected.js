e5ca2f6aac75aea2720549dd3642e7bc1bbb597b
Jeswin Kumar <jeswinpk@agilehead.com>
2017-04-21 08:22:00 +0530

    More convincing test for defer()

--- src/test/fixtures/defer/expected.js
+++ src/test/fixtures/defer/expected.js
@@ -1,4 +1,6 @@
 export const result = {
-  hello: { prop1: "one", counter: 1 },
-  world: { prop2: "two", counter: 0 }
+  hello: { prop1: "one", counter: 2 },
+  world: { prop2: "two", counter: 0 },
+  good: { prop3: "three", counter: 1 },
+  earth: { prop4: "four", counter: 3 }
 };
--- src/test/fixtures/defer/fixture.js
+++ src/test/fixtures/defer/fixture.js
@@ -4,7 +4,9 @@ let counter = 0;
 
 export const input = {
   hello: { prop1: "one" },
-  world: { prop2: "two" }
+  world: { prop2: "two" },
+  good: { prop3: "three" },
+  earth: { prop4: "four" }
 };
 
 export const schema = traverse({
@@ -19,6 +21,19 @@ export const schema = traverse({
     { prop2: capture() },
     {
       builders: [{ get: (_, { state }) => ({ ...state, counter: counter++ }) }]
+    }
+  ),
+  good: traverse(
+    { prop3: capture() },
+    {
+      builders: [{ get: (_, { state }) => ({ ...state, counter: counter++ }) }]
+    }
+  ),
+  earth: traverse(
+    { prop4: capture() },
+    {
+      builders: [{ get: (_, { state }) => ({ ...state, counter: counter++ }) }],
+      defer: true
     }
   )
 });
