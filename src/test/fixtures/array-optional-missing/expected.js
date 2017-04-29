85c3f6193a2d3dbf939a26a7402660d0c9b5d4bf
Jeswin Kumar <jeswinpk@agilehead.com>
2017-04-29 15:10:46 +0530

    WIP: simplification

---
+++ src/test/fixtures/array-optional-missing/expected.js
@@ -0,0 +1,3 @@
+export const result = {
+  level1: ["HELLO", true]
+};
---
+++ src/test/fixtures/array-optional-missing/fixture.js
@@ -0,0 +1,17 @@
+import {
+  traverse,
+  capture,
+  optionalItem,
+  array,
+  bool,
+  number,
+  string
+} from "../../../chimpanzee";
+
+export const input = {
+  level1: ["HELLO", true, 100]
+};
+
+export const schema = traverse({
+  level1: array([optionalItem(number()), string(), bool()])
+});
