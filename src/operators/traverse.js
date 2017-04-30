e822a223a03af5f4dc649df0cb220c80e62fdca0
Jeswin Kumar <jeswinpk@agilehead.com>
2017-04-30 10:21:57 +0530

    Make traverse just an operator

---
+++ src/operators/traverse.js
@@ -0,0 +1,31 @@
+/* @flow */
+import { getDefaultParams } from "../utils";
+import Schema from "../schema";
+
+import type {
+  ContextType,
+  InvokeType,
+  RawSchemaParamsType,
+  SchemaParamsType,
+  TaskType,
+  EnvType,
+  MetaType
+} from "./types";
+
+export function traverse(schema: Schema, rawParams: RawSchemaParamsType) {
+  const meta = { type: "traverse", schema, params: rawParams };
+  const params = { ...getDefaultParams(rawParams) };
+
+  function fn(obj, key, parents, parentKeys) {
+    return [
+      {
+        task: context =>
+          parseWithSchema(schema, meta, params)(obj, key, parents, parentKeys)(
+            context
+          )
+      }
+    ];
+  }
+
+  return new Schema(fn, params);
+}
