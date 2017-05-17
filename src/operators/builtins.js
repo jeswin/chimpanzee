d7383373e5563138eb79a36369e2357f7ea5cc2e
Jeswin Kumar <jeswinpk@agilehead.com>
2017-05-17 18:03:42 +0530

    Fix naming standards.

--- src/operators/builtins.js
+++ src/operators/builtins.js
@@ -10,22 +10,22 @@ import ObjectSchema from "../schemas/object";
 
 import { getParams } from "./utils";
 
-export function arr(literal, params) {
-  const meta = { type: "obj", literal, params };
-  return new ArraySchema(literal, getParams(params), meta);
+export function arr(schema, params) {
+  const meta = { type: "builtins.arr", schema, params };
+  return new ArraySchema(schema, getParams(params), meta);
 }
 
-export function func(literal, params) {
-  const meta = { type: "obj", literal, params };
-  return new FunctionSchema(literal, getParams(params), meta);
+export function func(schema, params) {
+  const meta = { type: "builtins.func", schema, params };
+  return new FunctionSchema(schema, getParams(params), meta);
 }
 
-export function val(literal, params) {
-  const meta = { type: "obj", literal, params };
-  return new PrimitiveSchema(literal, getParams(params), meta);
+export function val(schema, params) {
+  const meta = { type: "builtins.val", schema, params };
+  return new PrimitiveSchema(schema, getParams(params), meta);
 }
 
-export function obj(literal, params) {
-  const meta = { type: "obj", literal, params };
-  return new ObjectSchema(literal, getParams(params), meta);
+export function obj(schema, params) {
+  const meta = { type: "builtins.obj", schema, params };
+  return new ObjectSchema(schema, getParams(params), meta);
 }
