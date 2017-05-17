ff1d59562268937c8853d8427fbc7648a1acdd5e
Jeswin Kumar <jeswinpk@agilehead.com>
2017-05-17 19:06:19 +0530

    Add tests for nested property modifiers.

---
+++ src/test/fixtures/property-modifier-nested/expected.js
@@ -0,0 +1,3 @@
+export const result = {
+  level3: "hello"
+};
---
+++ src/test/fixtures/property-modifier-nested/fixture.js
@@ -0,0 +1,30 @@
+import { builtins as $, capture } from "../../../chimpanzee";
+
+export const input = {
+  getItem(item) {
+    return item === "level1"
+      ? {
+          getItem(item) {
+            return item === "level2"
+              ? {
+                  getItem(item) {
+                    return item === "level3" ? "hello" : "nothing";
+                  }
+                }
+              : "nothing";
+          }
+        }
+      : "nothing";
+  }
+};
+
+export const schema = $.obj(
+  {
+    level1: {
+      level2: {
+        level3: capture()
+      }
+    }
+  },
+  { modifiers: { property: (obj, key) => obj.getItem(key) } }
+);
