b18b2d9c5ecf3ba8d52b533b9e34c6fa02977445
Jeswin Kumar <jeswinpk@agilehead.com>
2017-06-13 08:23:43 +0530

    Capture skipped results in any()

--- src/operators/any.js
+++ src/operators/any.js
@@ -10,24 +10,29 @@ export function any(schemas, params) {
 
   function fn(obj, key, parents, parentKeys) {
     return context =>
-      (function run(schemas, nonMatching) {
+      (function run(schemas, skippedSchemas, skippedResults) {
         const result = parse(schemas[0])(obj, key, parents, parentKeys)(context);
         return result instanceof Match || result instanceof Empty || result instanceof Fault
           ? result
           : schemas.length > 1
-              ? run(schemas.slice(1), nonMatching.concat(schemas[0]))
-              : new Skip(
-                  "None of the items matched.",
-                  {
-                    obj,
-                    key,
-                    parents,
-                    parentKeys,
-                    nonMatching: nonMatching.concat(schemas[0])
-                  },
-                  meta
-                );
-      })(schemas, []);
+            ? run(
+                schemas.slice(1),
+                skippedSchemas.concat(schemas[0]),
+                skippedResults.concat(result)
+              )
+            : new Skip(
+                "None of the items matched.",
+                {
+                  obj,
+                  key,
+                  parents,
+                  parentKeys,
+                  skippedSchemas: skippedSchemas.concat(schemas[0]),
+                  skippedResults: skippedSchemas.concat(result)
+                },
+                meta
+              );
+      })(schemas, [], []);
   }
 
   return new FunctionSchema(fn, getParams(params), meta);
