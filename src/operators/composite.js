05dcdb9c3213f28fe0ffd47b0a697c27ed5da747
Jeswin Kumar <jeswinpk@agilehead.com>
2017-10-12 18:19:39 +0530

    Composite schema should discard empty arrays paths

--- src/operators/composite.js
+++ src/operators/composite.js
@@ -6,22 +6,32 @@ import parse from "../parse";
 import { getParams } from "./utils";
 
 function getSchema(schema, paramSelector) {
-  const schemaSelector = schema.params && schema.params.selector
-    ? schema.params.selector
-    : "default";
+  const schemaSelector =
+    schema.params && schema.params.selector
+      ? schema.params.selector
+      : "default";
 
   return Array.isArray(schema)
-    ? schema.map(item => getSchema(item, paramSelector)).filter(x => x !== undefined)
+    ? schema
+        .map(item => getSchema(item, paramSelector))
+        .filter(x => x !== undefined)
     : schema instanceof Schema
-        ? schemaSelector === paramSelector ? schema : undefined
-        : typeof schema === "object"
-            ? Seq.of(Object.keys(schema)).reduce((acc, key) => {
-                const result = getSchema(schema[key], paramSelector);
-                return result !== undefined && Object.keys(result).length > 0
-                  ? { ...acc, [key]: result }
-                  : acc;
-              }, {})
-            : paramSelector === "default" ? schema : undefined;
+      ? schemaSelector === paramSelector ? schema : undefined
+      : typeof schema === "object"
+        ? (() => {
+            const innerSchema = Seq.of(
+              Object.keys(schema)
+            ).reduce((acc, key) => {
+              const result = getSchema(schema[key], paramSelector);
+              return result !== undefined && Object.keys(result).length > 0
+                ? { ...acc, [key]: result }
+                : acc;
+            }, {});
+            return Object.keys(innerSchema).length > 0
+              ? innerSchema
+              : undefined;
+          })()
+        : paramSelector === "default" ? schema : undefined;
 }
 
 export function composite(schema, _paramsList, ownParams = {}) {
@@ -32,13 +42,21 @@ export function composite(schema, _paramsList, ownParams = {}) {
     ownParams
   };
 
-  const paramsList = _paramsList.some(params => !params.name || params.name === "default")
+  const paramsList = _paramsList.some(
+    params => !params.name || params.name === "default"
+  )
     ? _paramsList
     : [{ name: "default" }].concat(_paramsList);
 
   const schemas = paramsList.map(
-    params => new ObjectSchema(getSchema(schema, (params && params.name) || "default"), params)
+    params =>
+      new ObjectSchema(
+        getSchema(schema, (params && params.name) || "default"),
+        params
+      )
   );
+
+  debugger;
 
   function fn(obj, key, parents, parentKeys) {
     return context => {
@@ -50,11 +68,20 @@ export function composite(schema, _paramsList, ownParams = {}) {
 
       return schemas.length
         ? (function run([schema, ...rest], state) {
-            const result = parse(schema)(obj, key, parents, parentKeys)(context);
+            const result = parse(schema)(obj, key, parents, parentKeys)(
+              context
+            );
             return result instanceof Match || result instanceof Empty
               ? rest.length
-                  ? run(rest, result instanceof Match ? merge(state, result) : state)
-                  : new Match(result instanceof Match ? merge(state, result) : state, env, meta)
+                ? run(
+                    rest,
+                    result instanceof Match ? merge(state, result) : state
+                  )
+                : new Match(
+                    result instanceof Match ? merge(state, result) : state,
+                    env,
+                    meta
+                  )
               : result;
           })(schemas, {})
         : new Empty(env, meta);
