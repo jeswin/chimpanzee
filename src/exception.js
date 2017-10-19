b57c069170d9d2ccf87f893c8411d78e35a34193
Jeswin Kumar <jeswinpk@agilehead.com>
2017-10-19 18:53:14 +0530

    Descriptive errors

--- src/exception.js
+++ src/exception.js
@@ -1,4 +1,12 @@
-/*       */
-export default function(message) {
-  throw new Error(message);
+class ChimpanzeeError extends Error {
+  constructor(message, inner, schema) {
+    super(message);
+    this.inner = inner;
+    this.schema = schema;
+    this.name = "ChimpanzeeError";
+  }
+}
+
+export default function(message, inner, schema) {
+  throw new ChimpanzeeError(message, inner, schema);
 }
--- src/parse.js
+++ src/parse.js
@@ -7,7 +7,13 @@ import functionParser from "./parsers/function";
 import primitiveParser from "./parsers/primitive";
 import objectParser from "./parsers/object";
 
-import { ArraySchema, FunctionSchema, PrimitiveSchema, ObjectSchema, Schema } from "./schemas";
+import {
+  ArraySchema,
+  FunctionSchema,
+  PrimitiveSchema,
+  ObjectSchema,
+  Schema
+} from "./schemas";
 
 function getSchemaAndParser(source) {
   function normalize(src, SchemaClass, params = {}) {
@@ -18,12 +24,12 @@ function getSchemaAndParser(source) {
     ["string", "number", "boolean", "symbol"].includes(typeof source)
     ? { schema: normalize(source, PrimitiveSchema), parse: primitiveParser }
     : source instanceof FunctionSchema || source instanceof Function
-        ? { schema: normalize(source, FunctionSchema), parse: functionParser }
-        : source instanceof ArraySchema || source instanceof Array
-            ? { schema: normalize(source, ArraySchema), parse: arrayParser }
-            : source instanceof ObjectSchema || source.constructor === Object
-                ? { schema: normalize(source, ObjectSchema), parse: objectParser }
-                : exception(`Invalid schema type ${typeof source}.`);
+      ? { schema: normalize(source, FunctionSchema), parse: functionParser }
+      : source instanceof ArraySchema || source instanceof Array
+        ? { schema: normalize(source, ArraySchema), parse: arrayParser }
+        : source instanceof ObjectSchema || source.constructor === Object
+          ? { schema: normalize(source, ObjectSchema), parse: objectParser }
+          : exception(`Invalid schema type ${typeof source}.`);
 }
 
 /*
@@ -32,17 +38,26 @@ function getSchemaAndParser(source) {
 */
 export default function(source) {
   const { schema, parse } = getSchemaAndParser(source);
-  return (obj, key = "__UNKNOWN__", parents = [], parentKeys = []) => (_context = {}) => {
-    const context = schema.params && schema.params.reuseContext ? _context : {};
-    const result = parse(schema)(obj, key, parents, parentKeys)(context);
-    const build = schema.params && schema.params.build;
-    return build
-      ? (() => {
-          const output = build(obj, key, parents, parentKeys)(context)(result);
-          return output instanceof Result
-            ? output
-            : new Match(output, { obj, key, parents, parentKeys });
-        })()
-      : result;
+  return (obj, key = "__UNKNOWN__", parents = [], parentKeys = []) => (
+    _context = {}
+  ) => {
+    try {
+      const context =
+        schema.params && schema.params.reuseContext ? _context : {};
+      const result = parse(schema)(obj, key, parents, parentKeys)(context);
+      const build = schema.params && schema.params.build;
+      return build
+        ? (() => {
+            const output = build(obj, key, parents, parentKeys)(context)(
+              result
+            );
+            return output instanceof Result
+              ? output
+              : new Match(output, { obj, key, parents, parentKeys });
+          })()
+        : result;
+    } catch (ex) {
+      return exception(ex.message, ex, schema);
+    }
   };
 }
